6fefea0aa7d66b1b12c3b6b2f1895bf605f6928c
Ronaldo Lima <ronal2do@gmail.com>
2018-09-20 18:59:41 +0200

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+declare module 'rn-action-sheet' {
+  export function show(
+    options: Options,
+    callback?: (error?: any) => void
+  ): void;
+
+  export function showShare(
+    options: Options,
+    failureCallback?: (error?: any) => void,
+    successCallback?: (error?: any) => void,
+  ): void;
+
+  export interface Share {
+    url: String;
+    subject: String;
+    message: String;
+  }
+
+  export interface Options {
+    options: [String];
+    title: String;
+    dark: boolean;
+    cancelButtonIndex: number;
+  }
+}
